3ba57fb3bf6f7c35644c4b34a5e7558a9d9af236
deidg <64797316+deidg@users.noreply.github.com>
2024-05-28 11:04:33 +0300

    final commit

--- AlexBeerShop_MVP/Models/Networking/BeerListVcRequest.swift
+++ AlexBeerShop_MVP/Models/Networking/BeerListVcRequest.swift
@@ -1,5 +1,5 @@
 //
-//  NetworkRequest.swift
+//  BeerListVcRequest.swift
 //  AlexBeerShop_MVP
 //
 //  Created by Alex on 26.04.2024.
@@ -9,11 +9,12 @@ import Foundation
 
 protocol NetworkService {
     func getBeerList(from fileName: String, completion: (([SingleBeer]) -> Void)?)
-
+    func searchBeerByID(from fileName: String, id: Int, completion: @escaping (SingleBeer?) -> Void?)
+    func getRandomBeer(from fileName: String, completion: @escaping (SingleBeer?) -> Void?)
 }
 
-class NetworkRequest: NetworkService {
-    func getBeerList(from fileName: String, completion: (([SingleBeer]) -> Void)?) {
+class NetworkRequest: NetworkService {   // BeerListVcRequest.swift
+    func getBeerList(from fileName: String, completion: (([SingleBeer]) -> Void)?){
         if let sourceUrl = Bundle.main.url(forResource: fileName, withExtension: "json") {
             do {
                 let data = try Data(contentsOf: sourceUrl)
@@ -21,11 +22,33 @@ class NetworkRequest: NetworkService {
                 let jsonData = try decoder.decode(BeersModel.self, from: data)
                 completion?(jsonData.beers)
             } catch {
-                print("error: \(error.localizedDescription)")
+                print("error:\(error.localizedDescription)")
             }
         }
     }
- 
-    
-    
+    func searchBeerByID(from fileName: String, id: Int, completion: @escaping (SingleBeer?) -> Void?) {
+        if let sourcesuUrl = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: sourcesuUrl)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(BeersModel.self, from: data)
+                completion(jsonData.beers[id-1])
+            } catch {
+                print("error:\(error.localizedDescription)")
+            }
+        }
+    }
+    func getRandomBeer(from fileName: String, completion: @escaping (SingleBeer?) -> Void?) {
+        let randomInt = Int.random(in: 0..<25)
+        if let sourceUrl = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: sourceUrl)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(BeersModel.self, from: data)
+                completion(jsonData.beers[randomInt])
+            } catch {
+                print("error:\(error.localizedDescription)")
+            }
+        }
+    }
 }
